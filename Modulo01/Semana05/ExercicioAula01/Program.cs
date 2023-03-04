93d7f5591a2d79d2d164dec563798ab18837d03f
lucaskoerich <lucaskoerichsouza@hotmail.com>
2023-03-03 21:21:35 -0300

    Revert "Revert "Imp. Métodos e Classes""
    This reverts commit 3ceec16faa450397de39a135c5279c226f82bf23.

---
+++ Modulo01/Semana05/ExercicioAula01/Program.cs
@@ -0,0 +1,8 @@
+﻿using ExercicioAula01;
+
+Cliente juridico = new PessoaJuridica("rua Soares", "435", "ola", "000140");
+Cliente fisico = new PessoaFisica("rua Agosto", "555", "Lucas", "129");
+
+fisico.ExibirCliente();
+juridico.ExibirCliente();
+
