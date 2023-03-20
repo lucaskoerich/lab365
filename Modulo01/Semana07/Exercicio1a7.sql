70984009970370377e4555ae0975cf1bab7859f0
lucaskoerich <lucaskoerichsouza@hotmail.com>
2023-03-20 15:48:52 -0300

    Exercicio Semana 7

---
+++ Modulo01/Semana07/Exercicio1a7.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE EXERCICIO_SEMANA7
+GO
+USE EXERCICIO_SEMANA7
+
+-- Exercício 1
+
+CREATE TABLE CLIENTE(
+	ID INT,
+	NOME VARCHAR(50),
+	TELEFONE VARCHAR(12),
+	ENDERECO VARCHAR(50)
+)
+
+
+-- Exercício 2
+
+INSERT INTO CLIENTE(ID, NOME, TELEFONE, ENDERECO) VALUES (1, 'Vinicius Silva' , '987654', 'Rua Girassol'), 
+														 (2, 'Maria Antonia' , '123456' , 'Rua Rosas'), 
+														 (3,'Marcus Vinicius', '654123', 'Rua Itajai')
+
+
+-- Exercício 3
+
+SELECT * FROM CLIENTE
+
+
+-- Exercício 4
+
+SELECT * FROM CLIENTE WHERE ID = 1
+SELECT * FROM CLIENTE WHERE ID = 2
+SELECT * FROM CLIENTE WHERE ID = 3
+
+
+-- Exercício 5
+
+SELECT * FROM CLIENTE WHERE NOME LIKE 'Ma%'
+
+
+-- Exercício 6
+
+UPDATE CLIENTE SET ENDERECO = 'Rua do Limão' WHERE ID = 3
+
+
+-- Exercício 7
+
+DELETE CLIENTE WHERE ID = 2
