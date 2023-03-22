0485e2f2c1b3309f76c23a0a8a5c939aaeb6c369
lucaskoerich <lucaskoerichsouza@hotmail.com>
2023-03-22 20:40:42 -0300

    Adicionado Controllers

---
+++ Modulo01/Semana08/BasicAPI/Controllers/ClientesController.cs
@@ -0,0 +1,29 @@
+﻿using BasicAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAPI.Controllers
+{
+    [Route("clientes")]
+    public class ClientesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Metodo([FromQuery] string nome)
+        {
+            return Ok($"Olá, {nome}!");
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public string ObterClienteComId([FromQuery] string nome, [FromRoute] int id)
+        {
+            return $"Olá, {nome}! Seu ID é {id}.";
+        }
+
+        [HttpGet]
+        [Route("id/{id}")]
+        public ActionResult ObterCliente([FromQuery] string nome, [FromRoute] int id)
+        {
+            return Ok(new Cliente(id, nome));
+        }
+    }
+}
---
+++ Modulo01/Semana08/BasicAPI/Controllers/VeiculosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAPI.Controllers
+{
+    [Route("[controller]")]
+    public class VeiculosController : Controller
+    {
+        static List<string> carros = new() { { "Gol" }, { "Argo" }, { "Nivus" } };
+
+        [HttpGet]
+        public ActionResult GetCarros([FromQuery] string carro)
+        {
+            if (string.IsNullOrEmpty(carro))
+            {
+                return Ok(carros);
+            }
+
+            if (carros.Exists(x => x == carro))
+            {
+                return Ok("Carro cadastrado!");
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult PostCarros([FromQuery] string carro)
+        {
+            carros.Add(carro);
+            return Ok(carros);
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteCarros([FromQuery] string carro)
+        {
+            carros.Remove(carro);
+            return Ok();
+        }
+    }
+}
