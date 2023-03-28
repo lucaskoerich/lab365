74c5a27256c09d7509842fae657610d1d1e364c4
lucaskoerich <lucaskoerichsouza@hotmail.com>
2023-03-27 22:09:10 -0300

    Criando uma API

---
+++ Modulo01/Semana09/Aula01/Controllers/BandaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula01.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BandaController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult Post()
+    {
+        return Ok(true);
+    }
+}
