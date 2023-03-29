75923cc69a130d567c092a670643ab748a3ce09b
lucaskoerich <lucaskoerichsouza@hotmail.com>
2023-03-28 22:04:02 -0300

    Entity Framework

---
+++ Modulo01/Semana09/Aula02/Models/MesModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Aula02.Models;
+
+[Table("MES")]
+public class MesModel
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+    
+    [MaxLength(100)]
+    public string Nome { get; set; }
+    
+    
+    public int Ano { get; set; }
+    
+}
