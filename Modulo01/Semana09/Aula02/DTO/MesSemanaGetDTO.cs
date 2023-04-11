110059aaa3c9f690b81717635d15d7a38fd37b5c
lucaskoerich <lucaskoerichsouza@hotmail.com>
2023-04-11 13:53:52 -0300

    Adicionado DTOs

---
+++ Modulo01/Semana09/Aula02/DTO/MesSemanaGetDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula02.DTO;
+
+public class MesSemanaGetDTO
+{
+    public int IdMes { get; set; }
+    public string Mes { get; set; }
+    public List<MesSemanaGetDTO> SemanaGetDtos { get; set; }
+}
+
+public class SemanaGetDTO
+{
+    public int IdSemana { get; set; }
+    public int Dia { get; set; }
+    public string Observacao { get; set; }
+    
+}
